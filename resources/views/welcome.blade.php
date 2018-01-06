de53cba665e19ae7c111026a5981061e8d7ed268
James Miranda <jameswpm@gmail.com>
2018-01-06 21:43:39 -0200

    Create basic page (wrong references)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,65 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Aplicação Demo - BTM_Teste</title>
 
-        <title>Laravel</title>
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:600" type="text/css" rel="stylesheet" />
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+    <link href="style.css" type="text/css" rel="stylesheet" />
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="jquery.min.js"></script>
+    <script type="text/javascript" src="mapa.js"></script>
+    <script type="text/javascript" src="jquery-ui.custom.min.js"></script>
 
-            .position-ref {
-                position: relative;
-            }
+</head>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+<body>
 
-            .content {
-                text-align: center;
-            }
+<div class="container-fluid" id="main">
 
-            .title {
-                font-size: 84px;
-            }
+    <h1>BTM - Short Trip</h1>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    <form method="post" action="index.html">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+        <fieldset>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+            <legend>BTM - Short Trip</legend>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="col-md-12">
+                <div class="row">
+                    <div class=" col-md-6 form-group">
+                        <label for="add1" class="label-control">Partida:</label>
+                        <input type="text" id="add1" name="add1Text" class="form-control"/>
+                    </div>
+                    <div class="col-md-6 form-group">
+                        <label for="add2" class="label-control">Chegada:</label>
+                        <input type="text" id="add2" name="add2Text" class="form-control"/>
+                    </div>
+
+                    <div id="map"></div>
+
+                    <input type="submit" value="Obter menor caminho" name="btnSend" class="btn btn-primary"/>
+
+                    <input type="hidden" id="txtLatitude" name="txtLatitude" />
+                    <input type="hidden" id="txtLongitude" name="txtLongitude" />
                 </div>
             </div>
-        </div>
-    </body>
+
+        </fieldset>
+    </form>
+
+</div>
+
+</body>
+
+<footer>
+    <div><span>Desenvolvido por <a href="https://github.com/jameswpm" target="_blank">James Miranda.</a><br></span></div>
+</footer>
 </html>
