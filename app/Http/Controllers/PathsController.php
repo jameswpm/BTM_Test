96b3bd405ed0be0db188da650af7788921d939b7
James Miranda <jameswpm@gmail.com>
2018-01-07 01:46:12 -0200

    Testing Directions API

--- app/Http/Controllers/PathsController.php
+++ app/Http/Controllers/PathsController.php
@@ -16,6 +16,29 @@ class PathsController extends Controller
         $destinyLat = $request->get('txtLatitude2');
         $destinyLong = $request->get('txtLongitude2');
 
+        $response = \GoogleMaps::load('directions')
+            ->setParam ([
+                'origin' => $sourceLat . ',' . $sourceLong,
+                'destination' => $destinyLat . ',' . $destinyLong,
+                'mode' => 'driving',
+                'alternatives' => 'true'
+            ])
+            ->get();
+
+        //get response and define the 3 shortest paths (in time)
+        $routes = json_decode($response, true)['routes'];
+        $shortests_distances = [];
+        $shortests_durations = [];
+        foreach ($routes as $key => $route) {
+            $distance = $route['legs']['distance']['value'];
+            $duration = $route['legs']['duration']['value'];
+
+            if (!empty($shortests_distances) and count($shortests_distances) < 3) {
+
+            }
+        }
+
+
         return response()->json(['source' => 'Abigail', 'destiny' => 'CA']);
     }
 }
